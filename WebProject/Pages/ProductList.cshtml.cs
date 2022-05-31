35fdbd7824c7c7aa735978b2970236389a798180
oguz-kocer <68062849+oguz-kocer@users.noreply.github.com>
2022-05-31 08:46:27 +0300

    -DataTable sayfası & arayüzü oluşturuldu.

---
+++ WebProject/Pages/ProductList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebProject.Pages
+{
+    public class ProductListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
